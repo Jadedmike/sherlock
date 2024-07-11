a59271f0e0050bce78c51e1f212ac102c4ceaa76
Jadedmike <mhgwad161@gmail.com>
2024-07-11 09:44:32 -0700

    Update sherlock.py

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -1,3 +1,8 @@
 # This file used to trigger updates on packages too out of date
 # to use the new release tag format. Remove in next update.
 __version__ = "0.15.0"
+git clone https://github.com/sherlock-project/sherlock.git
+cd sherlock
+virtualenv .
+source bin/activate
+sherlock 42_vxw
